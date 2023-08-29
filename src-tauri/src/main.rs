d25bd03dff11cf1d1d4d0d203cf578adef393dc4
Jongho Hong <myodan@pm.me>
2023-08-29 21:47:03 +0900

    feat: tauri add shadows plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::Manager;
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .setup(|app| {
+            let win = app.get_window("main").unwrap();
+            #[cfg(not(target_os = "linux"))]
+            {
+                use window_shadows::set_shadow;
+                set_shadow(&win, true).unwrap();
+            }
+            Ok(())
+        })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
