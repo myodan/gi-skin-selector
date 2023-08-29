5d8f28d92171428cd1d850ee9f11219690c5561e
Jongho Hong <myodan@pm.me>
2023-08-29 22:15:12 +0900

    feat: panda css add

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,16 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import { css } from "~/styled-system/css";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>Hello 🐼!</Title>
+
+      <section>
+        <div class={css({ fontSize: "2xl", fontWeight: "bold" })}>
+          Hello 🐼!
+        </div>
+      </section>
     </main>
   );
 }
